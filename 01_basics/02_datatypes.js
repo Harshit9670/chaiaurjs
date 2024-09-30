b1686f2b773c11484325b82e1da9d565cffb91bf
Harshit9670 <133258060+Harshit9670@users.noreply.github.com>
2024-09-30 19:20:10 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)  //We are using node js ,not browsers
+console.log(3+3)
+console.log("Hitesh");
+
+let name="Hitesh"
+let age=18
+let isLoggedIn=false
+
+//number=>2 to power 53
+//Bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//symbol
