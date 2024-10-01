8f7d8c2d7b67aef7d7092ab7b8100a58a6cc9acc
Harshit9670 <133258060+Harshit9670@users.noreply.github.com>
2024-10-01 20:02:08 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_and_operations.js
@@ -0,0 +1,28 @@
+let score="33abc"
+console.log( typeof(score));
+
+let valueInNumber=Number(score)
+console.log( typeof(valueInNumber));
+
+
+console.log(valueInNumber);  //Here it gives NaN,which is a special type
+
+
+let val=33
+let strVal=String(val)
+console.log(typeof strVal);
+
+//"33"=>33
+//"33abc"=>NaN   not a number
+//true=>1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedin=Boolean(isLoggedIn)
+console.log(booleanIsLoggedin);
+
+
+//1=>true; 0=>false;
+//""=>false; "Harshit"=>true;
+
+
+
