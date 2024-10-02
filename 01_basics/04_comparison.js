2aeb2561ff4d3aaa86f071d713d465d031b07eea
Harshit9670 <133258060+Harshit9670@users.noreply.github.com>
2024-10-02 03:25:18 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+
+console.log("2">1);
+console.log("02">1);  //Always make compare between same data types because it doesnt gives predictable result
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)  //it doesn't giving prdictable result
+
+console.log(undefined ==0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+//strict checks ===  it strictly checks and also checks datatype
+
+console.log(null)
+console.log(typeof(null))
+
+
+
